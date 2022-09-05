42f1d899bc647974655d2c449ec0762a912ec61e
Travis=Prall <travis.prall667@gmail.com>
2022-09-05 09:25:44 -0700

    upgrade navbar

--- gitprofile/src/Components/navbar.js
+++ gitprofile/src/Components/navbar.js
@@ -1,38 +1,83 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
-import { Link } from 'react-router-dom';
+import { useLocation, NavLink, Outlet } from 'react-router-dom';
 
-class MainNav extends Component {
-  render() {
+function MainNav() {
+  const location = useLocation();
+  console.log(location);
+  return (
+    <Container fluid>
+      <Row>
+        <Navbar expand='sm' bg='dark' variant='dark' fixed='top'>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+          < MainTabs />
+            <Outlet />
+          </Navbar.Collapse>
+        </Navbar>
+      </Row>
+    </Container>
+  );
+}
+
+function MainTabs() {
+  const location = useLocation();
+  console.log(location);
+  if (location.pathname === '/') {
     return (
-      <Container fluid>
-        <Row>
-          <Navbar expand='sm' bg='dark' variant='dark' fixed='top'>
-            <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-            <Navbar.Collapse id='responsive-navbar-nav'>
-              <Nav className='justify-content-start' defaultActiveKey='#home'>
-                <Nav.Item as='li' className='me-auto'>
-                  <Nav.Link as={Link} to="/">Home</Nav.Link>
-                </Nav.Item>
-                <Nav.Item as='li' className='me-auto'>
-                  <Nav.Link href='#about'>About</Nav.Link>
-                </Nav.Item>
-                <Nav.Item as='li' className='me-auto'>
-                  <Nav.Link href='#resume'>Resume</Nav.Link>
-                </Nav.Item>
-                <Nav.Item as='li' className='me-auto'>
-                  <Nav.Link href='#contact'>Contact</Nav.Link>
-                </Nav.Item>
-                <Nav.Item as='li' className='me-auto'>
-                  <Nav.Link as={Link} to="/art">Art</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-        </Row>
-      </Container>
+      <Nav className='justify-content-start' defaultActiveKey='#home'>
+        <Nav.Item as='li' className='me-auto'>
+          <Nav.Link href='#home'>Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as='li' className='me-auto'>
+          <Nav.Link href='#about'>About</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as='li' className='me-auto'>
+          <Nav.Link href='#resume'>Resume</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as='li' className='me-auto'>
+          <Nav.Link href='#contact'>Contact</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as='li' className='me-auto'>
+          <Nav.Link as={NavLink} to='/art'>
+            Art
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+    );
+  } else if (location.pathname === '/art') {
+    return (
+      <Nav className='justify-content-start' defaultActiveKey='#photography'>
+        <Nav.Item as='li' className='me-auto'>
+          <Nav.Link as={NavLink} to='/'>
+            Home
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item as='li' className='me-auto'>
+          <Nav.Link href='#photography'>Photography</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as='li' className='me-auto'>
+          <Nav.Link href='#decor'>Decor</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    );
+  
+  } else {
+    return (
+      <Nav className='justify-content-start' defaultActiveKey='#home'>
+        <Nav.Item as='li' className='me-auto'>
+          <Nav.Link as={NavLink} to='/'>
+            Home
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item as='li' className='me-auto'>
+          <Nav.Link as={NavLink} to='/art'>
+            Art
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
     );
   }
 }
