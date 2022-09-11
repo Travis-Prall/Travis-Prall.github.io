6c72d4c2f1657b629b51c12076bca558e70674ea
Travis-Prall <smart@travisprall.com>
2022-09-11 09:24:24 -0700

    removed logs

--- gitprofile/src/app.js
+++ gitprofile/src/app.js
@@ -11,7 +11,6 @@ class App extends Component {
     this.state = {
       name: 'Travis Prall',
       resumeData: {},
-      // resumeData: profileData,
     };
   }
 
@@ -19,7 +18,6 @@ class App extends Component {
     ReactGA.initialize('UA-209754722-1');
     ReactGA.pageview(window.location.pathname + window.location.search);
     this.setState({ resumeData: profileData });
-    // console.log(this.state.resumeData);
   }
 
   render() {
