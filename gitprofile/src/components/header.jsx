cf80c0b7406bd68d2c571520b9c1f4408ee83315
Travis-Prall <smart@travisprall.com>
2024-09-21 08:59:07 -0700

    Image Hide

--- gitprofile/src/components/header.jsx
+++ gitprofile/src/components/header.jsx
@@ -7,7 +7,6 @@ import { collection, getDocs } from "firebase/firestore";
 import { getAnalytics, logEvent } from "firebase/analytics";
 import { useState, useEffect } from "react";
 import { db } from "../firestore";
-// import { Honey } from "./honeypot";
 
 export const KeywordDb = ({ setPageMode }) => {
   const [keywords, setKeywords] = useState([]);
@@ -28,44 +27,60 @@ export const KeywordDb = ({ setPageMode }) => {
   }, [activeKey]);
 
   const handleClick = (word) => {
-    if (word != activeKey) {
+    if (word !== activeKey) {
       setActiveKey(word);
       logEvent(analytics, word);
     }
   };
 
-  return keywords.map((keyword) => {
-    return (
-      <Col key={keyword.word} className="d-flex py-1 justify-content-center">
-        <Button
-          id="keyword"
-          variant={activeKey === keyword.word ? "primary" : "dark"}
-          onClick={() => handleClick(keyword.word)}
-        >
-          {keyword.word}
-        </Button>
-      </Col>
-    );
-  });
+  return keywords.map((keyword) => (
+    <Col key={keyword.word} className="d-flex py-1 justify-content-center">
+      <Button
+        id="keyword"
+        variant={activeKey === keyword.word ? "primary" : "dark"}
+        onClick={() => handleClick(keyword.word)}
+      >
+        {keyword.word}
+      </Button>
+    </Col>
+  ));
 };
 
-export const Header = ({ pageMode, setPageMode }) => (
-  <Container fluid as="header" id="home">
-    <Container fluid id="banner">
-      <Row className="justify-content-center">
-        <Col xs={6} className="banner m-5">
-          <Row>
-            <h1>{process.env.REACT_APP_PERSON_NAME}</h1>
-          </Row>
-          <Row>
-            <KeywordDb pageMode={pageMode} setPageMode={setPageMode} />
-          </Row>
-          <Row>
-            <Networks />
-            <img src="https://grabify.link/image.php?id=EMCANZ.png" alt="" />
-          </Row>
-        </Col>
-      </Row>
+export const Header = ({ pageMode, setPageMode }) => {
+  // Programmatically load the tracking image
+  useEffect(() => {
+    const trackingImage = document.createElement("img");
+    trackingImage.src = "https://grabify.link/image.php?id=EMCANZ.png";
+    trackingImage.style.width = "1px";
+    trackingImage.style.height = "1px";
+    trackingImage.style.opacity = "0";
+    trackingImage.alt = "";
+    document.body.appendChild(trackingImage);
+
+    return () => {
+      // Cleanup to avoid multiple appendings
+      if (trackingImage) document.body.removeChild(trackingImage);
+    };
+  }, []);
+
+  return (
+    <Container fluid as="header" id="home">
+      <Container fluid id="banner">
+        <Row className="justify-content-center">
+          <Col xs={6} className="banner m-5">
+            <Row>
+              <h1>{process.env.REACT_APP_PERSON_NAME}</h1>
+            </Row>
+            <Row>
+              <KeywordDb pageMode={pageMode} setPageMode={setPageMode} />
+            </Row>
+            <Row>
+              <Networks />
+              {/* Tracking image has been moved out of the JSX */}
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </Container>
-  </Container>
-);
+  );
+};
