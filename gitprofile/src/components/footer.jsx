9db8d0acddf8d3a859b0278480e36380de047196
Travis-Prall <smart@travisprall.com>
2026-01-17 17:59:51 -0700

    refactor: remove unused import of SocialMediaBar in Footer component

--- gitprofile/src/components/footer.jsx
+++ gitprofile/src/components/footer.jsx
@@ -2,7 +2,6 @@ import { memo } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { SocialMediaBar } from "./social";
 
 const Footer = memo(() => {
   const currentYear = new Date().getFullYear();
